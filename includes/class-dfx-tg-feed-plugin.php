6c7267c16ac28d9130c696102795fdab6143433f
David Marín <292309+davefx@users.noreply.github.com>
2025-11-05 10:44:06 +0100

    Update includes/class-dfx-tg-feed-plugin.php with latest implementation

--- includes/class-dfx-tg-feed-plugin.php
+++ includes/class-dfx-tg-feed-plugin.php
@@ -23,7 +23,7 @@ final class Plugin {
         // Autoload or require class files
         spl_autoload_register(function($class) {
             if (strpos($class, __NAMESPACE__) === 0) {
-                $name = strtolower(str_replace('\\', '-', str_replace(__NAMESPACE__ . '\\', '', $class)));
+                $name = strtolower(str_replace('\', '-', str_replace(__NAMESPACE__ . '\', '', $class)));
                 $file = __DIR__ . '/class-' . $name . '.php';
                 if (file_exists($file)) { require $file; }
             }
@@ -33,6 +33,7 @@ final class Plugin {
         add_action('init',          [$this, 'register_blocks']);
         add_action('admin_menu',    [$this, 'settings_page']);
         add_action('admin_init',    [$this, 'register_settings']);
+        add_action('wp_ajax_dfx_tg_feed_test', [Settings::instance(), 'ajax_test_bot_channel']);
         add_action('wp_ajax_dfx_tg_feed_refresh', [Cache::instance(), 'ajax_refresh_cache']);
     }
 
