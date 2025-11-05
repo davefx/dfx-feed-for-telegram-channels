bde1b2fc04e2e5ade1468120e20bed23faadfb9c
David Marín <292309+davefx@users.noreply.github.com>
2025-11-05 11:37:51 +0100

    Create dfx-telegram-channel-feed.php file

---
+++ dfx-telegram-channel-feed.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: DFX Telegram Channel Feed
+Description: Display messages from a Telegram channel on your WordPress site using your Telegram Bot.
+Version: 1.0.0
+Author: David Marín
+Author URI: http://www.davefx.com
+Text Domain: dfx-tg-feed
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'DFX_TG_FEED_PATH', plugin_dir_path( __FILE__ ) );
+define( 'DFX_TG_FEED_URL', plugin_dir_url( __FILE__ ) );
+define( 'DFX_TG_FEED_VER', '1.0.0' );
+
+require_once DFX_TG_FEED_PATH . 'includes/class-dfx-tg-feed-plugin.php';
+
+add_action( 'plugins_loaded', function() {
+    \DFX\TelegramChannelFeed\Plugin::instance();
+} );
